3d2dc7d2bc7c22edeebe34cce910f3843f64c939
Garrett Smith <g@rre.tt>
2014-11-22 12:55:07 +0000

    Server work

--- src/buildstuff.erl
+++ src/buildstuff.erl
@@ -1,6 +1,8 @@
 -module(buildstuff).
 
--export([start/0, hello/0, req_socket/2, send/2]).
+-export([start/0, hello/0,
+         req_socket/2, rep_socket/1,
+         send/2, recv/1, rep_loop/1]).
 
 start() ->
     e2_application:start_with_dependencies(buildstuff).
@@ -13,7 +15,21 @@ req_socket(Addr, Port) ->
     ok = ezmq:connect(Req, tcp, Addr, Port, []),
     Req.
 
+rep_socket(Port) ->
+    {ok, Rep} = ezmq:socket([{type, rep}]),
+    ok = ezmq:bind(Rep, tcp, Port, []),
+    Rep.
+
 send(Socket, Bin) when is_binary(Bin) ->
     ezmq:send(Socket, [Bin]);
 send(Socket, Frames) when is_list(Frames) ->
     ezmq:send(Socket, Frames).
+
+recv(Socket) ->
+    ezmq:recv(Socket).
+
+rep_loop(Rep) ->
+    {ok, Result} = ezmq:recv(Rep),
+    io:format("Got: ~p~n", [Result]),
+    ezmq:send(Rep, Result),
+    rep_loop(Rep).
